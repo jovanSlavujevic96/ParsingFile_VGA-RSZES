c96400b626feb0bf8c9bcece25e717b1f9f9fc43
jana <jana.peric@gmail.com>
2019-11-03 19:16:26 +0000

    first commit

--- prvi_zadatak.c
+++ prvi_zadatak.c
@@ -14,6 +14,9 @@
 #define RED 0xf800
 #define GREEN 0x07e0
 
+#define MAX_STR_SIZE 1000
+#define NUM_OF_LINES 5 
+
 void fill(int *error, int x, int y, int color, FILE *fp);
 void fillBgr(int *error, const int color);
 void fillHorizLine(int *error, const int color, const int X1, const int X2, const int Y);
@@ -21,60 +24,113 @@ void fillVertLine(int *error, const int color, const int X, const int Y1, const 
 void fillRectangle(int *error, const int color, const int X1, const int X2, const int Y1, const int Y2);
 int retColor(const char *argument);
 
-int main(int argc, char **argv)
+int main(void)
 {
-	bool background = false;
-	int color, out;
-	if(argc != 3 && argc != 6 && argc != 7)
+	int out;
+	FILE *text_file;
+	const char *path = "/root/RSZES_PrviZadatak/command.txt";
+	char str[MAX_STR_SIZE];
+	
+	text_file = fopen(path, "r");
+	if(text_file == NULL) 
 	{
-		printf("Pogresan unos.\nPrimer unosa:\n%s BCKG: BLACK\n",argv[0]);
+		printf("Could not open file %s\n",path);
 		return -1;
-	}	
-	int bckgCmp = strcmp(argv[1], "BCKG:"),
-	lineHCmp = strcmp(argv[1], "LINE_H:"),
-	lineVCmp = strcmp(argv[1], "LINE_V:"),
-	rectCmp = strcmp(argv[1], "RECT:");
+	}
+	int i=0;
+	char commands[NUM_OF_LINES][MAX_STR_SIZE];
+	while(fgets(str,MAX_STR_SIZE, text_file) != NULL)
+	{
+		printf("%d: %s",i, str);
+		strcpy(commands[i], str);
+		++i;
+	}
+	fclose(text_file);
+
+	i = 0;
+	while(i < NUM_OF_LINES && out != -1)
+	{
+		char arg[6][100] = {{0}};	
+		int j,k, flag[5];
+		bool kljuc[6] = {false};
+		for(j=0; j<strlen(commands[i]);++j)
+		{
+			if(!kljuc[0] && commands[i][j] != 32)
+				arg[0][j] = commands[i][j];
+			else if(!kljuc[0] && commands[i][j] == 32)
+			{
+				flag[0] = j;
+				kljuc[0] = true;
+			}
+			for(k=1; k<5; k++)
+			{
+				if(kljuc[k-1] && !kljuc[k] && flag[k-1] != j && commands[i][j] != 32)
+					arg[k][j-flag[k-1]-1] = commands[i][j];
+				else if(kljuc[k-1] && !kljuc[k] && flag[k-1] != j && commands[i][j] == 32)
+				{
+					flag[k] = j;
+					kljuc[k] = true;
+				}
+			}	
+			if(kljuc[4] && !kljuc[5] && flag[4] != j && commands[i][j] != 32 && commands[i][j] != 10)
+				arg[5][j-flag[4]-1] = commands[i][j];
+		}
+		int color;
+		int bckgCmp = strcmp(arg[0], "BCKG:"),
+		lineHCmp = strcmp(arg[0], "LINE_H:"),
+		lineVCmp = strcmp(arg[0], "LINE_V:"),
+		rectCmp = strcmp(arg[0], "RECT:");
 	
-	if(!bckgCmp)
-	{
-		color = retColor(argv[2]);
-		fillBgr(&out, color);
+		if(!bckgCmp)
+		{
+			printf("%s", arg[1]);
+			arg[1][strlen(arg[1])-1] = 0;
+			printf("%s", arg[1]);
+			color = retColor(arg[1]);
+			fillBgr(&out, color);
+		}
+		else if(!lineHCmp)
+		{
+			int x1 = atoi(arg[1]),
+			x2 = atoi(arg[2]),
+			y  = atoi(arg[3]);
+			arg[4][strlen(arg[4])-1] = 0;
+			color = retColor(arg[4]);
+			fillHorizLine(&out, color, x1, x2, y);	
+		}
+		else if(!lineVCmp)
+		{
+			int x = atoi(arg[1]),
+			y1 = atoi(arg[2]),
+			y2 = atoi(arg[3]);
+			arg[4][strlen(arg[4])-1] = 0;
+			color = retColor(arg[4]);
+			fillVertLine(&out, color, x, y1, y2);
+		}
+		else if(!rectCmp)
+		{
+			int x1 = atoi(arg[1]),
+			x2 = atoi(arg[2]),
+			y1 = atoi(arg[3]),
+			y2 = atoi(arg[4]);
+			arg[5][strlen(arg[5])-1] = 0;
+			color = retColor(arg[5]);
+			fillRectangle(&out, color, x1, x2, y1, y2);	
+		}
+		else
+		{
+			printf("Na %d liniji %s fajla nepravilan unos komande\n", i, path);
+			out = -1;
+		}
+		printf("Izvrseno\n");
+		usleep(2000000); 
+		++i;
 	}
-	
-	if(!lineHCmp)
-	{
-		int x1 = atoi(argv[2]),
-		x2 = atoi(argv[3]),
-		y  = atoi(argv[4]);
-		color = retColor(argv[5]);
-		fillHorizLine(&out, color, x1, x2, y);	
-	}
-
-	if(!lineVCmp)
-	{
-		int x = atoi(argv[2]),
-		y1 = atoi(argv[3]),
-		y2 = atoi(argv[4]);
-		color = retColor(argv[5]);
-		fillVertLine(&out, color, x, y1, y2);
-	}
-
-	if(!rectCmp)
-	{
-		int x1 = atoi(argv[2]),
-		x2 = atoi(argv[3]),
-		y1 = atoi(argv[4]),
-		y2 = atoi(argv[5]);
-		color = retColor(argv[6]);
-		fillRectangle(&out, color, x1, x2, y1, y2);	
-	}
-
-	return out;
+	return 0;
 }
 
 void fillBgr(int *error, const int color)
 {
-
 	unsigned int vectorColor[480][640];
 	int i,j;
 	for(i=0; i<480; ++i)
@@ -153,6 +209,7 @@ void fillRectangle(int *error, const int color, const int X1, const int X2, cons
 
 int retColor(const char *argument)
 {
+	printf("%s", argument);
 	int color;
 	int blueCmp = strcmp(argument,"BLUE"), 
 	blackCmp = strcmp(argument, "BLACK"), 
